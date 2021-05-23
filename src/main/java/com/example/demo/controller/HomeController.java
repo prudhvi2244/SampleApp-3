262dea1ade077d713cb64ba703d839ac9f5d364e
prudhvi4399 <prudhvi88801@gmail.com>
2021-05-23 18:46:10 +0530

    Added New EndPoint

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -11,5 +11,10 @@ public class HomeController {
 	{
 		return "<h1 style='color:red;text-align:center'>Welcome To SampleApp-3</h1>";
 	}
+	@GetMapping(value = "/about")
+	public String aboutPage()
+	{
+		return "<h1 style='color:blue;text-align:center'>Welcome To SampleApp-3 - About Page</h1>";
+	}
 	
 }
